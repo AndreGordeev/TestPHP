0abbbf33d036683587da559faf824520d1a9e3c9
andre <andregrdv@aol.com>
2023-09-24 18:51:34 +0400

    first

---
+++ Index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AndreGordeevForTestUse</title>
+    <link rel="icon" href="images\favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-- importing header -->
+
+    <?php
+    include "header.html"
+        ?>
+    <hr>
+
+    <!-- Content here -->
+
+
+    <hr>
+    <!-- importing footer -->
+    <?php
+    include "footer.html"
+        ?>
+
+</body>
+
+</html>
---
+++ content.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- Content -->
+    <main class="main">
+        <div class="main-content responsive-wrapper">
+            <article class="widget">
+                <h2>Here is the title</h2>
+                <p>
+
+
+
+                </p>
+            </article>
+        </div>
+    </main>
+
+</body>
+
+</html>
